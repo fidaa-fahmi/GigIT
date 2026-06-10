f9618a2e90881069c2c3eb1aa996ce35aacbea47
fidaa-fahmi <fidaaahmadfahmi@gmail.com>
2026-06-10 12:16:29 +0800

    Update : Update dummy database and API

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import {createRoot} from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 import { AuthProvider } from './context/AuthContext';
+import 'leaflet/dist/leaflet.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
